80764418dca9c18719a3e9651cd9a4789a204fd2
Almrg <111406898+Almrg@users.noreply.github.com>
2022-12-01 12:33:36 -0600

    Add files via upload

---
+++ proyecto/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.proyecto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnScan;
+    EditText txtResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnScan = findViewById(R.id.btnScan);
+        txtResultado = findViewById(R.id.txtResultado);
+
+
+        btnScan.setOnClickListener(v -> {
+            IntentIntegrator integrador = new IntentIntegrator(MainActivity.this);
+            integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+            integrador.setPrompt("Lector - CDP");
+            integrador.setCameraId(0);
+            integrador.setBeepEnabled(true);
+            integrador.setBarcodeImageEnabled(true);
+            integrador.initiateScan();
+        });
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+                txtResultado.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
